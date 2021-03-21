b4180a23177a27e816fcfcd6d76b6386c550f263
Babak Bigdeli <babakbigdeli@gmail.com>
2021-03-21 16:12:04 +0000

    Installing service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,12 @@
+// https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API
+// https://developers.google.com/web/ilt/pwa/introduction-to-service-worker
+// https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+// Lifecycle of a Service Worker
+// The lifecycle of a service worker is completely separated from your web page one. It consists of the following phases:
+// Registration 
+// Installation
+// Activation
+
 const FILES_TO_CACHE = [
     "/", 
     "/index.html", 
@@ -9,3 +18,17 @@ const FILES_TO_CACHE = [
   
   const CACHE_NAME = "static-cache-v2";
   const DATA_CACHE_NAME = "data-cache-v1";
+
+// the service worker is already registered in index.html by the time its directed here.
+
+// Installing service worker
+
+self.addEventListener("install", function (event) {
+    event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        return cache.addAll(FILES_TO_CACHE);
+      })
+    );
+    self.skipWaiting();
+  });
+
